8280ea54b448a333c8095febd1f17868675f254e
Gleb <demonxklimenko@mail.ru>
2021-06-10 17:54:17 +0300

    Commit Message

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -3,6 +3,7 @@ package ru.netology.service;
 import org.testng.annotations.Test;
 import static org.testng.Assert.*;
 
+
 public class CashbackHackServiceTest {
     CashbackHackService service = new CashbackHackService();
 

7bbbcfa2090780699dffc2532996060534fed575
Gleb <demonxklimenko@mail.ru>
2021-07-08 22:27:13 +0300

    Commit Message

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -8,21 +8,21 @@ import static org.junit.Assert.assertEquals;
 public class CashbackHackServiceTest {
     CashbackHackService service = new CashbackHackService();
 
-    @Test
+    @org.junit.Test
     public void shouldAmountLessThan1000() {
         int actual = service.remain(900);
         int expected = 100;
         assertEquals(expected, actual);
     }
 
-    @Test
+    @org.junit.Test
     public void shouldAmountMoreThan1000() {
         int actual = service.remain(1100);
         int expected = 900;
         assertEquals(expected, actual);
     }
 
-    @Test
+    @org.junit.Test
     public void shouldAmount1000() {
         int actual = service.remain(1000);
         int expected = 0;
